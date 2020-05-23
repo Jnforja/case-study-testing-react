a6ab4da2e0c0293bc0c3d3997178c3a304bfd508
João Forja <jnforja@gmail.com>
2020-05-23 17:14:52 +0100

    Add test file and test list

---
+++ src/__test__/shows-characters.test.js
@@ -0,0 +1,1 @@
+test("Mock test", function test() {});
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import { RickAndMortyCharactersPage } from "./RickAndMortyCharacters";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RickAndMortyCharactersPage />
   </React.StrictMode>,
   document.getElementById("root")
 );
