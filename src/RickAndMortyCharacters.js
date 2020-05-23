ab83b504bc86beb0c61ff86b294843e9477e044c
João Forja <jnforja@gmail.com>
2020-05-23 17:45:48 +0100

    Show loading message while waiting for characters

--- src/RickAndMortyCharacters.js
+++ src/RickAndMortyCharacters.js
@@ -1,15 +1,25 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+
+const LOADING_STATUS = "LOADING";
+const SHOWING_CHARACTERS_STATUS = "SHOWING_CHARACTERS";
 
 function RickAndMortyCharactersPage({ fetchCharacters }) {
+  const [status, setStatus] = useState(LOADING_STATUS);
+
   useEffect(
     function fetchCharactersOnStart() {
-      fetchCharacters();
+      fetchCharacters().then(function updateStateToShowCharacters() {
+        setStatus(SHOWING_CHARACTERS_STATUS);
+      });
     },
     [fetchCharacters]
   );
   return (
     <div>
-      <p>There aren't characters to show</p>
+      {status === LOADING_STATUS && <p>Loading...</p>}
+      {status === SHOWING_CHARACTERS_STATUS && (
+        <p>There aren't characters to show</p>
+      )}
     </div>
   );
 }
--- src/__test__/shows-characters.test.js
+++ src/__test__/shows-characters.test.js
@@ -1,5 +1,9 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
 import { RickAndMortyCharactersPage } from "../RickAndMortyCharacters";
 
 test("Shows empty message when there aren't characters", async function test() {
@@ -8,5 +12,13 @@ test("Shows empty message when there aren't characters", async function test() {
   expect(
     await screen.findByText("There aren't characters to show")
   ).toBeVisible();
+  expect(fetchCharacters).toHaveBeenCalledWith();
+});
+
+test("Shows loading message while waiting for characters", async function test() {
+  const fetchCharacters = jest.fn().mockResolvedValueOnce([]);
+  render(<RickAndMortyCharactersPage fetchCharacters={fetchCharacters} />);
+  expect(screen.getByText("Loading...")).toBeVisible();
+  await waitForElementToBeRemoved(() => screen.getByText("Loading..."));
   expect(fetchCharacters).toHaveBeenCalledWith();
 });
