4ea2a780927846e0e74436c88d2886a03cae6abd
João Forja <jnforja@gmail.com>
2020-05-23 22:14:33 +0100

    Updates

---
+++ precache-manifest.7e60bf68593059582d404f5f72b32854.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e93f9bd969908142dd54e8c80f56920",
+    "url": "/case-study-testing-react/index.html"
+  },
+  {
+    "revision": "19b88f0109315a1d1ddd",
+    "url": "/case-study-testing-react/static/css/main.1aef1b7b.chunk.css"
+  },
+  {
+    "revision": "50438b5f998768d9daf6",
+    "url": "/case-study-testing-react/static/js/2.9bd57c24.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/case-study-testing-react/static/js/2.9bd57c24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19b88f0109315a1d1ddd",
+    "url": "/case-study-testing-react/static/js/main.79845388.chunk.js"
+  },
+  {
+    "revision": "96f8c549244abe852ec5",
+    "url": "/case-study-testing-react/static/js/runtime-main.a14c09df.js"
+  },
+  {
+    "revision": "1cad9376d80daa166ad18124b7b6cdb7",
+    "url": "/case-study-testing-react/static/media/get_schwifty-webfont.1cad9376.woff2"
+  },
+  {
+    "revision": "979169ba8dd64329d220a11f09af8144",
+    "url": "/case-study-testing-react/static/media/get_schwifty-webfont.979169ba.woff"
+  }
+]);
